7418e2414ac0c1bfa54f7065a4e1ee0a9a4b7289
= <=>
2022-12-19 21:56:54 -0300

    Adicionado querySelector

---
+++ querySelector/script.js
@@ -0,0 +1,5 @@
+const query_divTodas = [...document.querySelectorAll("div > p")];
+const query_cursosTodos = [...document.querySelectorAll(".cursos")];
+const query_cursocs1 = [...document.querySelectorAll(".c1,p")];
+const query_cursosc2 = [...document.querySelectorAll(".c2")];
+const query_cursoEspecial = [...document.querySelectorAll("#c2")];
