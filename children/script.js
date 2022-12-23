c1f637e09b0ed83d0310feada6f253f0fc141f42
= <=>
2022-12-22 21:54:08 -0300

    children

---
+++ children/script.js
@@ -0,0 +1,8 @@
+const caixa = document.querySelector("#caixa");
+const btn = document.querySelector(".curso");
+
+console.log(document.getRootNode());
+
+console.log(caixa.firstElementChild)
+console.log(caixa.lastElementChild)
+console.log(caixa.children)
