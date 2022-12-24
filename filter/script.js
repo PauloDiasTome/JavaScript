c8e22411fc29011daf10603142e4938dfa925e96
= <=>
2022-12-24 17:45:25 -0300

    array filter

---
+++ filter/script.js
@@ -0,0 +1,12 @@
+const filterMaior18 = (valor) => {
+    if (valor > 18) {
+        return valor;
+    }
+}
+
+const idades = [15, 21, 30, 17, 18];
+const maior = idades.filter((val, ind, arry) => {
+    if (valor > 18) {
+        return valor;
+    }
+})
