9dbab4db84972db2501489ef0e76cebf9bbdac72
= <=>
2023-01-01 10:01:19 -0300

    Toggle

--- toggle/script.js
+++ toggle/script.js
@@ -1,6 +1,9 @@
 const newCurso = () => {
     const newElm = document.createElement("div");
     newElm.addEventListener("click", function () {
-        evt.target.classList.target("selecionado")
-    })
+        evt.target.classList.toggle("selecionado")
+    });
 }
+
+//  Toggle remove a class selecionado caso a class exista
+//  Ou adicionada se não existir
