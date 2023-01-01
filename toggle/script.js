3f71015cf72b6f937c7451b16d19de5b3d806c50
= <=>
2023-01-01 09:53:10 -0300

    Toggle

---
+++ toggle/script.js
@@ -0,0 +1,6 @@
+const newCurso = () => {
+    const newElm = document.createElement("div");
+    newElm.addEventListener("click", function () {
+        evt.target.classList.target("selecionado")
+    })
+}
