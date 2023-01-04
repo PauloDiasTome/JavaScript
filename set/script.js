15c55384e2960e3cb977331ef636606c83a73d60
= <=>
2023-01-03 21:20:01 -0300

    Set

---
+++ set/script.js
@@ -0,0 +1,7 @@
+let musicas = new Array(["musica", "louvor", "hino", "hino"]);
+console.log(musicas);
+
+let musicass = new Set(["musica", "louvor", "hino", "hino"]);
+musicas.add("musica01");
+musicas.add("musica01");
+console.log(musicass);
