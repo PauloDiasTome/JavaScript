16ca576955cef12862bac8aabac87316ea4b8c74
= <=>
2023-03-22 20:57:55 -0300

    stact

---
+++ conversion_OBJ/script.js
@@ -0,0 +1,51 @@
+const Pessoa = {
+    nome: "Bruno",
+    sobrenome: "Silva",
+    idade: 23,
+    sexo: "M"
+}
+
+
+console.log(JSON.stringify(Pessoa));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
